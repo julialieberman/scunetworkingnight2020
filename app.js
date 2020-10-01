d57b0645eba5f801a9480f0b6ae8038a0a34d5a5
julialiebs <jlieberman@scu.edu>
2020-09-30 18:16:01 -0700

    moved to gh-pages

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+// my scripts
+//connect to iothub
+
+var app = express();
+const server= require('http').createServer(app);
+
+const bodyParser = require('body-parser');
+const { request, response } = require('express');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('./'))
+
+
+
+// render the html pages. the '/<name>' is what the url extension will look like in the browser
+app.get('/', function (req, res) {
+    res.render('index.html')
+})
+
+
+
+/** on page error */
+app.use(function (req, res, next) 
+{
+    res.status(404).send("404 not found. :(");
+})
+
+server.listen(5000, () => console.log('App listening on http://localhost:5000')); //listen to port 5000
